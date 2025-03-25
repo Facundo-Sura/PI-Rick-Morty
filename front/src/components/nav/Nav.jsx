6bf44086d8bb13dfb3436c12cfcb463fffd3520c
Facundo Sura <facundomesura@gmail.com>
2025-03-25 16:58:23 -0300

    estilos para la navbar y searchbar

--- front/src/components/nav/Nav.jsx
+++ front/src/components/nav/Nav.jsx
@@ -1,13 +1,24 @@
-import { Link } from 'react-router-dom'
-import SearchBar from '../searchbar/SearchBar'
+import { Link } from "react-router-dom";
+import SearchBar from "../searchbar/SearchBar";
+import style from "./Nav.module.css";
 
-function Nav({onSearch}){
-    return <div>
-        <Link to='/home'><button >Home</button></Link>
-        <Link to='/about'><button>About</button></Link>
-        <Link to='/favorites'><button>Favorites</button></Link>
-        <SearchBar onSearch={onSearch}/>
+function Nav({ onSearch }) {
+  return (
+    <div className={style.navbar}>
+      <div className={style.menu}>
+        <Link to="/home" className={style.item}>
+          <button className={style.item}>Home</button>
+        </Link>
+        <Link to="/about" className={style.item}>
+          <button className={style.item}>About</button>
+        </Link>
+        <Link to="/favorites" className={style.item}>
+          <button className={style.item}>Favorites</button>
+        </Link>
+      </div>
+      <SearchBar onSearch={onSearch} />
     </div>
+  );
 }
 
-export default Nav
+export default Nav;
--- front/src/components/searchbar/SearchBar.jsx
+++ front/src/components/searchbar/SearchBar.jsx
@@ -1,22 +1,28 @@
 import { useState } from "react";
+import style from "./Search.module.css";
 
 export default function SearchBar(props) {
-   
-   const [id, setId] = useState('')
+  const [id, setId] = useState("");
 
-   function handleChange(evento) {
-      setId(evento.target.value)
-   }
+  function handleChange(evento) {
+    setId(evento.target.value);
+  }
 
-   const search = () => {
-      props.onSearch(id)
-      setId('')
-   }
+  const search = () => {
+    props.onSearch(id);
+    setId("");
+  };
 
-   return (
-      <div>
-         <input type='search' onChange={handleChange} placeholder="Ingresa un ID" value={id}/>
-         <button onClick={search}>Agregar</button>
-      </div>
-   );
+  return (
+    <div className={style.bar}>
+      <input
+        type="search"
+        onChange={handleChange}
+        placeholder="Ingresa un ID"
+        value={id}
+         className={style.input}
+      />
+      <button onClick={search} className={style.add}>Agregar</button>
+    </div>
+  );
 }
